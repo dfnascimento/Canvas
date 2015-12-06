516f8f5d5203ccc18f05abb717c12650a585a613
Aline Jordão <alihhj@gmail.com>
2015-12-06 13:48:56 -0200

    .

--- Desktop/Program.cs
+++ Desktop/Program.cs
@@ -16,7 +16,7 @@ namespace Desktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Teste());
+           // Application.Run(new Participante());
         }
     }
 }
