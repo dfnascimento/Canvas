22ab22c8f01f060e83252284ba63074629eedb64
dfnascimento <df.nascimento93@gmail.com>
2015-12-06 17:28:50 -0200

    UC1, UC2, UC3

---
+++ Modelo/PN/PNAvaliadorExterno.cs
@@ -0,0 +1,51 @@
+﻿using Modelo.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.PN
+{
+    public class PNAvaliadorExterno
+    {
+
+        public String nome { get; set; }
+        public String email { get; set; }
+        public byte[] senha { get; set; }
+        public String areaAtuacao { get; set; }
+        public String formacao { get; set; }
+        public String status { get; set; }
+        
+        public bool Inserir()
+        {
+
+            CanvasEntities2 db = new CanvasEntities2();
+
+            Avaliador_Externo a = new Avaliador_Externo();
+            Participante p = new Participante();
+
+            p.Nome = this.nome;
+            p.Senha = this.senha;
+            p.Email = this.email;
+            p.Status = "Inativo";
+
+            //Insere no banco
+
+            db.Participantes.Add(p);
+            db.SaveChanges();
+
+            a.Id_Participante = db.Participantes.Where(s => s.Email == p.Email).FirstOrDefault<Participante>().Id;
+
+            a.Area_Atuacao = this.areaAtuacao;
+            a.Formacao = this.formacao;
+
+            db.Avaliador_Externo.Add(a);
+            db.SaveChanges();
+
+            return true;
+
+        }
+
+    }
+}
