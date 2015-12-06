8121fe888a3b2db3162020e72e84ab59335abd2f
dfnascimento <df.nascimento93@gmail.com>
2015-12-06 17:35:03 -0200

    UC1, UC2, UC3

---
+++ Modelo/PN/PNAluno.cs
@@ -0,0 +1,52 @@
+﻿using Modelo.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.PN
+{
+    public class PNAluno
+    {
+        public String nome { get; set; }
+        public String email { get; set; }
+        public byte[] senha { get; set; }
+        public String curso { get; set; }
+        public int periodo { get; set; }
+        public String campus { get; set; }
+        public String status { get; set; }
+
+
+        public bool Inserir()
+        {
+
+            DAO.CanvasEntities2 db = new CanvasEntities2();
+
+            Participante p = new Participante();
+            Aluno al = new Aluno();
+
+            p.Nome = this.nome;
+            p.Senha = this.senha;
+            p.Email = this.email;
+            p.Status = "Ativo";
+
+            //Insere no banco
+
+            db.Participantes.Add(p);
+            db.SaveChanges();
+
+            al.Id_Participante = db.Participantes.Where(s => s.Email == p.Email).FirstOrDefault<Participante>().Id;
+            al.Periodo = this.periodo;
+            al.Campus = this.campus;
+            al.Curso = this.curso;
+            
+            db.Alunoes.Add(al);
+            db.SaveChanges();
+
+            return true;
+
+        }
+
+    }
+}
