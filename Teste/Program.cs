b1de5750d4823289c0c00a6c712e161b7d407cff
dfnascimento <df.nascimento93@gmail.com>
2015-12-07 18:42:54 -0200

    Commit

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -12,17 +12,45 @@ namespace Teste
     {
         static void Main(string[] args)
         {
-            Participante p = new Participante();
-            Professor pr = new Professor();
+            //Participante p = new Participante();
+            //Professor pr = new Professor();
 
-            p.Nome = "Teste";
-            p.Email = "teste";
-            p.Status = "novo";
-            p.Senha = System.Text.Encoding.UTF8.GetBytes("teste");
-            pr.Departamento = "dainf";
-            pr.Disciplina_Principal = "quimica";
+            //p.Nome = "Teste";
+            //p.Email = "teste";
+            //p.Status = "novo";
+            //p.Senha = System.Text.Encoding.UTF8.GetBytes("teste");
+            //pr.Departamento = "dainf";
+            //pr.Disciplina_Principal = "quimica";
 
-            CadastroProfessor.Inserir(pr, p);
+            //CadastroProfessor.Inserir(pr, p);
+
+            //List<PNProfessor> lista = CadastroProfessor.ListaProfessoresInativos()
+
+            //PNProfessor pn = new PNProfessor();
+            //pn.nome = "Diego de Faria do Nascimento";
+            //pn.email = "df.nascimento93@gmail.com";
+            //pn.senha = System.Text.Encoding.UTF8.GetBytes("tste");
+            //pn.disciplinaPrincipal = "Matemática";
+            //pn.departamento = "Física";
+
+            //Console.Write("teste");
+            //CadastroProfessor.Inserir(pn);
+
+            //PNProfessor pn2 = new PNProfessor();
+            //pn2.nome = "João Silva";
+            //pn2.email = "df.teste@gmail.com";
+            //pn2.senha = System.Text.Encoding.UTF8.GetBytes("tste");
+            //pn2.disciplinaPrincipal = "Algoritmo";
+            //pn2.departamento = "Informatica";
+
+            //CadastroProfessor.Inserir(pn2);
+
+
+            //foreach (var prof in CadastroProfessor.ListaProfessoresInativos()) {
+
+            //    Console.Write(prof.nome + " " + prof.email + " " + prof.status + "\n");
+            //}
+
         }
     }
 }
