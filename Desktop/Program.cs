45075f0fc3bf5c278a95ac0fdc9afb807fc4b342
dfnascimento <df.nascimento93@gmail.com>
2015-12-07 18:40:33 -0200

    Web

--- Desktop/Program.cs
+++ Desktop/Program.cs
@@ -16,7 +16,7 @@ namespace Desktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-           // Application.Run(new Participante());
+            Application.Run(new Canvas());
         }
     }
 }
