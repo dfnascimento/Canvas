b4a5b55ba24d531d81dc25cfaad233df282e2de4
Aline Jordão <alihhj@gmail.com>
2015-12-13 00:30:33 -0200

    Web Controlls

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Descrição";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Página para Contato.";
 
             return View();
         }
